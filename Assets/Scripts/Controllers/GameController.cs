960047a14612b64148464d065396d24bb76af431
Clara Gavinho <clara.gavinho@gmail.com>
2022-10-05 17:32:19 -0300

    Mr Enoki on Click

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -11,13 +12,14 @@ public class GameController : MonoBehaviour
     
     public void PlayNextStoryScene(){
 
+        bottomBar.GetComponent<RectTransform>().localScale = new Vector3(0.6f,1f,1f);
         bottomBar.PlayScene(currentScene);
 
     }
 
     void Start()
     {
-        PlayNextStoryScene();
+        //PlayNextStoryScene();
     }
 
     void Update()
